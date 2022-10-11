2ea8c0b111b5cfd1dd80d773c4cd6339b7ca5ca6
muhdi15 <115169961+muhdi15@users.noreply.github.com>
2022-10-11 16:54:42 +0530

    perkenalan python
    perkenalan python

---
+++ perkenalan python.py
@@ -0,0 +1,14 @@
+# ini adalah program Perkenalan
+
+ayah= "Masdar"
+ibu= "Safitri"
+anak1= "Muhdi"
+anak2= "Madina"
+anak3= "Maulida"
+anak4= "Syarif"
+anak5= "Fauzi"
+anak6= "Ramadan"
+
+print(f"Namaku adalah {anak1} dan aku tinggal Di polewali, Saya lulus tahun 2022 dari Sma 3 Polewali")
+      
+print(f"aku punya 5 saudara \n.1 {anak2}\n.2 {anak3} \n.3 {anak4} \n.4 {anak5} \n.5 {anak6}")
